8a0d1d12cbd0a2d0f55fcaa8f12c21db7dcea657
Pranavkhade <jerniganlab@kdh-335-103.bb.iastate.edu>
2018-08-01 17:31:35 -0500

    Parser iterative capture.

--- Pranav.py
+++ Pranav.py
@@ -1,28 +1,59 @@
 '''
 Author: Pranav Khade(pranavk@iastate.edu)
+SourceofInformation: https://www.cgl.ucsf.edu/chimera/docs/UsersGuide/tutorials/pdbintro.html
 '''
+import numpy
 
 class Atom():
-    def __init__(self):
-        return True
+    def __init__(self,id):
+        self.id=id
+        self.AtomName=None
+        self.AlternateLocationIndicator=None #remove it later
+        self.ResidueName=None #get parent
+        self.Chain=None
+        self.Coordinates=None
+        self.Occupancy=None
+        self.bfactors=None
+        self.SegmentIdentifier=None
+        self.Element=None
 
 class AminoAcid():
     def __init__(self):
-        return True
+        self.id=None
+        self.name=None
+        self.Chain=None
 
 class Chain():
     def __init__(self):
-        return True
+        self.id=None
+        self.name=None
 
-class Frame():
+class Model():
     def __init__(self):
-        return True
+        print "hello world"
 
 class Protein():
     def __init__(self):
-        return True
+        print "Hello world"
 
 def main():
+    fh=open('5mti.pdb')
+    for _ in fh:
+        if(_[0:4]=='ATOM'):
+            Serial=int(_[6:11])
+            AtomName=_[12:16].strip()
+            AlternateLocationIndicator=_[16]
+            ResidueName=_[17:20]
+            ChainID=_[21]
+            ResidueNumber=_[22:26].strip()
+            CodeForInsertions=_[26]
+            Coordinates=numpy.array([float(_[30:38]),float(_[38:46]),float(_[46:54])])
+            Occupancy=float(_[54:60])
+            bfactors=float(_[60:66])
+            SegmentIdentifier=_[72:76]
+            Element=_[76:78].strip()
+            Charge=_[78:80]
+            
     return True
 
 if(__name__=='__main__'):
